0935f19738d82f36cddb4d54ae7a09ad198755ba
hongmaoxiao <769501294@qq.com>
2023-01-06 07:50:29 +0800

    add day1 store whole numbers

--- day1.playground/Contents.swift
+++ day1.playground/Contents.swift
@@ -41,3 +41,24 @@ print(result.uppercased())
 print(movie.hasPrefix("A day"))
 
 print(filename.hasSuffix(".jpg"))
+
+let score = 10
+let reallyBig = 1_00_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 2
+let doubledScore = score * 2
+let squareScore = score * score
+let halveScore = score / 2
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
