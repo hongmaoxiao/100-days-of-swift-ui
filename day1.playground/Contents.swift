d45210f3a6fa4d886f5aa5b5040edd734e6947c5
hongmaoxiao <769501294@qq.com>
2023-01-06 07:22:07 +0800

    add day1 create strings

--- day1.playground/Contents.swift
+++ day1.playground/Contents.swift
@@ -20,3 +20,24 @@ print(playerName)
 let managerName = "Michael Scott"
 let dogBreed = "Samoyed"
 let meaningOfLife = "How many roads must a man walk down?"
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
