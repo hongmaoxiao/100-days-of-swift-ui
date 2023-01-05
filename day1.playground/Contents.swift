d65e2be8d23558cfd957b61bc88ab28b3c174573
hongmaoxiao <769501294@qq.com>
2023-01-06 06:21:28 +0800

    add day1 create variables and constants

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
