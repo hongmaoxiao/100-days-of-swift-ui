f9ae380a44120bef22db27be593bab0704cbecc2
hongmaoxiao <769501294@qq.com>
2023-01-07 02:08:37 +0800

    add day5 use switch statements to check multiple conditions

--- day5.playground/Contents.swift
+++ day5.playground/Contents.swift
@@ -147,3 +147,84 @@ if transport == .airplane || transport == .helicopter {
 } else {
     print("I'm going to hire a scooter now!")
 }
+
+
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+if forecast == .sun {
+    print("It should be a nice day.")
+} else if forecast == .rain {
+    print("Pack en umbrella.")
+} else if forecast == .wind {
+    print("Wear something warm")
+} else if forecast == .rain {
+    print("School is cancelled.")
+} else {
+    print("Our forecast generator is broken!")
+}
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack en umbrella.")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken!")
+}
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("Your're Batman!")
+case "Mega-City One":
+    print("Your're Judge Dredd!")
+case "Wakanda":
+    print("Your're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+let day = 5
+print("My true love gave to me...")
+
+switch day {
+case 5:
+    print("5 golden rings")
+case 4:
+    print("4 calling birds")
+case 3:
+    print("3 French hens")
+case 2:
+    print("2 turtle doves")
+default:
+    print("A partridge in a pear tree")
+}
+
+let day1 = 5
+print("My true love gave to me...")
+
+switch day1 {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
