5765fc5be7731a1de2c39b66b1d79ab20daad455
hongmaoxiao <769501294@qq.com>
2023-01-06 08:51:43 +0800

    add day2 join strings together

--- day2.playground/Contents.swift
+++ day2.playground/Contents.swift
@@ -13,3 +13,25 @@ print(isAuthenticated)
 var gameOver = false
 gameOver.toggle()
 print(gameOver)
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+//let missionMessage = "Apollo " + String(number) + " landed on the moon"
+let missionMessage = "Apollo \(number) landed on the moon"
+
+print("5 x 5 is \(5 * 5)")
