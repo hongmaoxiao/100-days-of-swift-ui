d1df5ab0571c481cc79b8af4eab7b3d602889180
hongmaoxiao <769501294@qq.com>
2023-01-07 07:04:32 +0800

    add day7 reuse code with functions

---
+++ day7.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+func printTimesTables(number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(number: 5)
+
+func printTimesTablesWithEnd(number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTablesWithEnd(number: 5, end: 20)
