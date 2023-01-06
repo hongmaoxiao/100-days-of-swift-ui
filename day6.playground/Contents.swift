fe72d283d516df0fbad82bc85692fdaac980fb9d
hongmaoxiao <769501294@qq.com>
2023-01-07 06:31:20 +0800

    add day6 Checkpoint 3

--- day6.playground/Contents.swift
+++ day6.playground/Contents.swift
@@ -112,3 +112,16 @@ for i in 1...100_000 {
 }
 
 print(multiples)
+
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
