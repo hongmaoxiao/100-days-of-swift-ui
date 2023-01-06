a829cbbdfa3670b3a682cfe81f7ca832909373c5
hongmaoxiao <769501294@qq.com>
2023-01-07 05:57:26 +0800

    add day6 use a while loop to repeat work

--- day6.playground/Contents.swift
+++ day6.playground/Contents.swift
@@ -10,6 +10,10 @@ for platform in platforms {
     print("Swift works great on \(platform).")
 }
 
+print(platforms[1...3])
+
+print(platforms[0...])
+
 for i in 1...12 {
     print("5 x \(i) is \(5 * i)")
 }
@@ -42,3 +46,22 @@ for _ in 1...5 {
 }
 
 print(lyric)
+
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off!")
+
+var roll = 0
+
+while roll != 20 {
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+print("Critical hit!")
