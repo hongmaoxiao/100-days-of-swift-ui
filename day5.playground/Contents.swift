e52cc43e99cfbd953be1182622e1af4d23828e38
hongmaoxiao <769501294@qq.com>
2023-01-07 01:00:36 +0800

    add day5 check multiple conditions

--- day5.playground/Contents.swift
+++ day5.playground/Contents.swift
@@ -75,3 +75,75 @@ if username.isEmpty {
     username = "Anonymous"
 }
 
+enum Sizes: Comparable {
+    case small
+    case medium
+    case large
+}
+
+let first = Sizes.small
+let second = Sizes.large
+
+print(first < second)
+
+let age1 = 16
+if age1 >= 18 {
+    print("You can vote in the next election")
+}
+
+if age1 < 18 {
+    print("Sorry, you're too young to vote.")
+}
+
+if age1 >= 18 {
+    print("You can vote in the next election")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let a = false
+let b = true
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+let temp = 25
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day.")
+    }
+}
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
+}
+
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path...")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
