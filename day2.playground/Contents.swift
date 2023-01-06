4809b69027d4bef9c1ee27d12d46c2fbb96708e8
hongmaoxiao <769501294@qq.com>
2023-01-06 08:35:29 +0800

    add day2 store truth with Booleans

---
+++ day2.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+let goodDogs = true
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameOver = false
+gameOver.toggle()
+print(gameOver)
