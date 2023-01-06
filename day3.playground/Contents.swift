9d70b58105b36ccb6c34625b151aa060197cdec4
hongmaoxiao <769501294@qq.com>
2023-01-06 11:04:24 +0800

    add day3 store and find data in dictionaries

--- day3.playground/Contents.swift
+++ day3.playground/Contents.swift
@@ -60,3 +60,44 @@ let presidents = ["Bush", "Obama", "Trump", "Biden"]
 print(presidents)
 let reversePresidents = presidents.reversed()
 print(reversePresidents)
+
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+let employee2 = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
+
+print(employee2["name"])
+print(employee2["job"])
+print(employee2["location"])
+
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+print(employee2["age", default: "Unknown"])
+
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo",
+]
+print(olympics[2012, default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+archEnemies["Superman"] = "Penguin"
+print(archEnemies["Superman", default: "Unknown"])
