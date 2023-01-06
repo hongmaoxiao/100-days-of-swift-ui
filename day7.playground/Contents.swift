1fefc11eee4fdf2c8d63806672e19324d743f86b
hongmaoxiao <769501294@qq.com>
2023-01-07 07:36:01 +0800

    add day7 return values from functions

--- day7.playground/Contents.swift
+++ day7.playground/Contents.swift
@@ -15,3 +15,52 @@ func printTimesTablesWithEnd(number: Int, end: Int) {
 }
 
 printTimesTablesWithEnd(number: 5, end: 20)
+
+
+let root = sqrt(169)
+print(root)
+
+func rollDice() -> Int {
+    return Int.random(in: 1...6)
+}
+
+func rollDice1() -> Int {
+    Int.random(in: 1...6)
+}
+
+let result = rollDice()
+print(result)
+
+print(rollDice1())
+
+func areLettersIdentical(string1: String, string2: String) -> Bool {
+    let first = string1.sorted()
+    let second = string2.sorted()
+    return first == second
+}
+
+print(areLettersIdentical(string1: "abc", string2: "acb"))
+
+func areLettersIdentical1(string1: String, string2: String) -> Bool {
+    return string1.sorted() == string2.sorted()
+}
+
+print(areLettersIdentical1(string1: "abc", string2: "acb"))
+
+func areLettersIdentical2(string1: String, string2: String) -> Bool {
+    string1.sorted() == string2.sorted()
+}
+
+print(areLettersIdentical2(string1: "abc", string2: "acb"))
+
+func pythagoras(a: Double, b: Double) -> Double {
+    let input = a * a + b * b
+    let root = sqrt(input)
+    return root
+}
+print(pythagoras(a: 3, b: 4))
+
+func pythagoras1(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+print(pythagoras1(a: 3, b: 4))
