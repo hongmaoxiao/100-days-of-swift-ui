535cfbe27448127a1420ad4e07f816d4074ecccd
hongmaoxiao <769501294@qq.com>
2023-01-06 08:09:43 +0800

    add day1 store decimal numbers

--- day1.playground/Contents.swift
+++ day1.playground/Contents.swift
@@ -62,3 +62,23 @@ counter /= 2
 let number = 120
 print(number.isMultiple(of: 3))
 print(120.isMultiple(of: 3))
+
+
+let num = 0.1 + 0.2
+print(num)
+
+let a = 1
+let b = 2.0
+// let c = a + Int(b)
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var n = "Nicolas Cage"
+n = "John Travolta"
+
+var rating = 5.0
+rating *= 2
