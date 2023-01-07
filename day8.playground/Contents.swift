6523b90e480698e7e5b4179f502448829bfa1bd3
hongmaoxiao <769501294@qq.com>
2023-01-07 10:52:35 +0800

    add day8 handle errors in functions

--- day8.playground/Contents.swift
+++ day8.playground/Contents.swift
@@ -21,3 +21,42 @@ var characters = ["Lana", "Pam", "Ray", "Sterling"]
 print(characters.count)
 characters.removeAll(keepingCapacity: true)
 print(characters.count)
+
+
+
+// How to handle errors in functions
+
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    
+    if password.count < 8 {
+        return "Ok"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let string = "12345"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
