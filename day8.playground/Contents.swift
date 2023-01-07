df5fd6a40acdc2b93fb84fd9f32c65bd0920d1c5
hongmaoxiao <769501294@qq.com>
2023-01-07 10:23:31 +0800

    add day8 provide default values for parameters

---
+++ day8.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+func printTimesTables(for number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+
+func printTimesTables1(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables1(for: 5, end: 20)
+printTimesTables1(for: 8)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+characters.removeAll(keepingCapacity: true)
+print(characters.count)
