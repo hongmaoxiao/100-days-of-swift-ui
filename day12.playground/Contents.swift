e9c6e09c70930f96b2c251887f41508cf3e97d93
hongmaoxiao <769501294@qq.com>
2023-01-09 04:39:14 +0800

    add day12 create your own classes

---
+++ day12.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+// How to create your own classes
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var newGame = Game()
+newGame.score += 10
