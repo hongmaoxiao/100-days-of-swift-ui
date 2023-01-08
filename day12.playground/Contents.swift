a7e35a58480cddf81038260e083bed7e1bfa7b29
hongmaoxiao <769501294@qq.com>
2023-01-09 05:22:30 +0800

    add day12 add initializers for classes

--- day12.playground/Contents.swift
+++ day12.playground/Contents.swift
@@ -49,3 +49,26 @@ joseph.work()
 
 let novall = Developer(hours: 8)
 novall.printSummary()
+
+
+// How to add initializers for classes
+class Vehicle {
+    let isElectric: Bool
+    
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+
+class Car: Vehicle {
+    let isConvertible: Bool
+    
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
+    }
+}
+
+let teslaX = Car(isElectric: true, isConvertible: false)
+print(teslaX.isConvertible)
+print(teslaX.isElectric)
