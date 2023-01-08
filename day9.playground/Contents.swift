8c27285f7ebd40c0eacafbce6e2775af6ebf4382
hongmaoxiao <769501294@qq.com>
2023-01-08 11:13:48 +0800

    add day9 Checkpoint 5

--- day9.playground/Contents.swift
+++ day9.playground/Contents.swift
@@ -142,3 +142,9 @@ doImportantWork {
 } third: {
     print("This is the third work")
 }
+
+
+// Checkpoint 5
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+luckyNumbers.filter({ $0 % 2 != 0 }).sorted { $0 < $1 }.map({ print("\($0) is a lucky number") })
+
