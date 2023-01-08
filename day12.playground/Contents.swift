2adb67dd515f014723c7c2a16eec3f064f305f2d
hongmaoxiao <769501294@qq.com>
2023-01-09 04:53:12 +0800

    add day12 make one class inherit from another

--- day12.playground/Contents.swift
+++ day12.playground/Contents.swift
@@ -11,3 +11,41 @@ class Game {
 
 var newGame = Game()
 newGame.score += 10
+
+
+// How to make one class inherit from another
+class Employee {
+    let hours: Int
+    
+    init(hours: Int) {
+        self.hours = hours
+    }
+    
+    func printSummary() {
+        print("I work \(hours) hours a day.")
+    }
+}
+
+class Developer: Employee {
+    func work() {
+        print("I'm writing code for \(hours) hours.")
+    }
+    
+    override func printSummary() {
+        print("I'm a developer who will sometimes work \(hours) hours a day, but other times spend hours arguing about whether code should be indented using tabs or spaces.")
+    }
+}
+
+class Manager: Employee {
+    func work() {
+        print("I'm going to meetings for \(hours) hours.")
+    }
+}
+
+let robert = Developer(hours: 8)
+let joseph = Manager(hours: 10)
+robert.work()
+joseph.work()
+
+let novall = Developer(hours: 8)
+novall.printSummary()
