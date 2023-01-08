5d31bc66363d4e7067c305edbba810e7e4dd48ae
hongmaoxiao <769501294@qq.com>
2023-01-09 03:29:23 +0800

    add day11 Static properties and methods

--- day11.playground/Contents.swift
+++ day11.playground/Contents.swift
@@ -27,3 +27,32 @@ if success {
 } else {
     print("Failed to get the money")
 }
+
+
+// Static properties and methods
+struct School {
+    static var studentCount = 0
+    
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Taylor Swift")
+print(School.studentCount)
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "setting.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+struct Employee {
+    let uername: String
+    let password: String
+    
+    static let example = Employee(uername: "cfederighi", password: "hairforceone")
+}
+
+print(Employee.example.uername)
