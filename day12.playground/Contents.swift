74e514544420be8bab6436156f4b0be62cec210b
hongmaoxiao <769501294@qq.com>
2023-01-10 05:14:13 +0800

    add day12 copy classes

--- day12.playground/Contents.swift
+++ day12.playground/Contents.swift
@@ -72,3 +72,40 @@ class Car: Vehicle {
 let teslaX = Car(isElectric: true, isConvertible: false)
 print(teslaX.isConvertible)
 print(teslaX.isElectric)
+
+
+// How to copy classes
+class User {
+    var username = "Anonymous"
+}
+
+var user1 = User()
+var user2 = user1
+user2.username = "Taylor"
+print(user1.username)
+print(user2.username)
+
+struct UserSturct {
+    var username = "Anonymous"
+}
+
+var user3 = UserSturct()
+var user4 = user3
+user3.username = "Taylor"
+print(user3.username)
+print(user4.username)
+
+class UserCopy {
+    var username = "Anonymous"
+    
+    func copy() -> User {
+        let user = User()
+        user.username = username
+        return user
+    }
+}
+var user5 = UserCopy()
+var user6 = user5.copy()
+user5.username = "Taylor"
+print(user5.username)
+print(user6.username)
