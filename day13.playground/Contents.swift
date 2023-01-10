7f1381ec4b43995e41c425d3f8ad1ed1ffad50f9
hongmaoxiao <769501294@qq.com>
2023-01-10 17:44:55 +0800

    add day13  create and use extensions

--- day13.playground/Contents.swift
+++ day13.playground/Contents.swift
@@ -71,3 +71,62 @@ func getRandomBool() -> some Equatable {
 
 print(getRandomNumber() == getRandomNumber())
 print(getRandomBool() == getRandomBool())
+
+
+// How to create and use extensions
+var quote = "  The truth is rarely pure and never simple   "
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+print(trimmed)
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+let trimmed1 = quote.trimmed()
+print(trimmed1)
+
+quote.trim()
+print(quote)
+
+let lyrics = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+print(lyrics.lines)
+print(lyrics.lines.count)
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+    
+//    init(title: String, pageCount: Int) {
+//        self.title = title
+//        self.pageCount = pageCount
+//        self.readingHours = pageCount / 50
+//    }
+}
+
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178)
+print(lotr.title)
+print(lotr.readingHours)
