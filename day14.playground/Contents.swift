164b8d18722956d91b5e8f37a3a3c89ed0a00cfc
hongmaoxiao <769501294@qq.com>
2023-01-12 11:27:19 +0800

    add day14  handle multiple optionals using optional chaining

--- day14.playground/Contents.swift
+++ day14.playground/Contents.swift
@@ -85,3 +85,20 @@ print(author)
 let names1 = ["Taylor", "Paul", "Adele"]
 let lengthOfLast = names1.last?.count
 print(lengthOfLast)
+
+
+// handle function failure with optionals
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user1 = (try? getUser(id: 23)) ?? "Anonymous"
+print(user1)
