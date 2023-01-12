660479a0f5840f76d4e1dbbef7c368ad1b545f64
hongmaoxiao <769501294@qq.com>
2023-01-12 09:23:57 +0800

    add day14 handle multiple optionals using optional chaining

--- day14.playground/Contents.swift
+++ day14.playground/Contents.swift
@@ -41,3 +41,17 @@ var myVar: Int? = 3
 if let unwrapped = myVar {
     print("Run if myVar has a value inside")
 }
+
+
+// How to handle multiple optionals using optional chaining
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+struct Book {
+    let title: String
+    let author: String?
+}
+var book: Book? = nil
+let author = book?.author?.first?.uppercased() ?? "A"
+print(author)
