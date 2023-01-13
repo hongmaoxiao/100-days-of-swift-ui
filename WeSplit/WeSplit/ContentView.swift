ec3e247f6106d4f60204895a7a5c0be843d09b02
hongmaoxiao <769501294@qq.com>
2023-01-13 22:51:37 +0800

    add day16 Creating views in a loop

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ''
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        Form {
-            TextField("Enter your name: ", text: $name)
-            Text("Your name is \(name)")
+        NavigationView{
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
         }
     }
 }
