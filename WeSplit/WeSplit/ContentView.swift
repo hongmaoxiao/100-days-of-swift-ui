95fdb5d6ca813b0fba2530dffaaaef89f33221de
hongmaoxiao <769501294@qq.com>
2023-01-13 14:08:27 +0800

    add day16  Adding a navigation bar

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by xhm on 2023/1/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUi")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by xhm on 2023/1/13.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
