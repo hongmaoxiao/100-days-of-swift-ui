4a1fa7441fa6524238594478794163a79a5e148a
hongmaoxiao <769501294@qq.com>
2023-01-14 22:40:37 +0800

    add day20 Using stacks to arrange views

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by xhm on 2023/1/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by xhm on 2023/1/14.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
