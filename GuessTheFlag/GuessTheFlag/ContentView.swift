c1baf8fb7af8ade15f35789d85275c6d1019dafa
hongmaoxiao <769501294@qq.com>
2023-01-15 21:29:45 +0800

    add day21 Stacking up buttons

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,37 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+//    @State private var showingAlert = false
     var body: some View {
-//        Button("Delete selection", role: .destructive, action: execteDeleting)
-//        VStack {
-//            Button("Button 1") {}
-//                .buttonStyle(.bordered)
-//            Button("Button 2", role: .destructive) {}
-//                .buttonStyle(.bordered)
-//            Button("Button 3") {}
-//                .buttonStyle(.borderedProminent)
-//                .tint(.green)
-//            Button("Button 4", role: .destructive) {}
-//                .buttonStyle(.borderedProminent)
-//        }
-//        Button {
-//            print("Button was tapped!")
-//        } label: {
-//            //Image(systemName: "pencil")
-//            Label("Edit", systemImage: "pencil")
-//        }
+        //        Button("Delete selection", role: .destructive, action: execteDeleting)
+        //        VStack {
+        //            Button("Button 1") {}
+        //                .buttonStyle(.bordered)
+        //            Button("Button 2", role: .destructive) {}
+        //                .buttonStyle(.bordered)
+        //            Button("Button 3") {}
+        //                .buttonStyle(.borderedProminent)
+        //                .tint(.green)
+        //            Button("Button 4", role: .destructive) {}
+        //                .buttonStyle(.borderedProminent)
+        //        }
+        //        Button {
+        //            print("Button was tapped!")
+        //        } label: {
+        //            //Image(systemName: "pencil")
+        //            Label("Edit", systemImage: "pencil")
+        //        }
         
-//        Image(systemName: "pencil")
+        //        Image(systemName: "pencil")
         
-        Button("Show Alert") {
-            showingAlert = true
-        }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("Delete", role: .destructive) {}
-            Button("Cancel", role: .cancel) {}
-        } message: {
-            Text("Please read this")
+        //        Button("Show Alert") {
+        //            showingAlert = true
+        //        }
+        //        .alert("Important message", isPresented: $showingAlert) {
+        //            Button("Delete", role: .destructive) {}
+        //            Button("Cancel", role: .cancel) {}
+        //        } message: {
+        //            Text("Please read this")
+        //        }
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
         }
     }
     
