285a01d0acf7b749019f994de854113082fdde3f
hongmaoxiao <769501294@qq.com>
2023-01-15 12:36:08 +0800

    add day20 Buttons and images

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+//        Button("Delete selection", role: .destructive, action: execteDeleting)
+//        VStack {
+//            Button("Button 1") {}
+//                .buttonStyle(.bordered)
+//            Button("Button 2", role: .destructive) {}
+//                .buttonStyle(.bordered)
+//            Button("Button 3") {}
+//                .buttonStyle(.borderedProminent)
+//                .tint(.green)
+//            Button("Button 4", role: .destructive) {}
+//                .buttonStyle(.borderedProminent)
+//        }
+        Button {
+            print("Button was tapped!")
+        } label: {
+            //Image(systemName: "pencil")
+            Label("Edit", systemImage: "pencil")
+        }
+        
+//        Image(systemName: "pencil")
+    }
+    
+    func execteDeleting() {
+        print("Now deleting")
     }
 }
 
