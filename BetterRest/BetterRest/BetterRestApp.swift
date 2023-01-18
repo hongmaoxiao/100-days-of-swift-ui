99635132b50d168fa8384507b3e7028e264c5aa5
hongmaoxiao <769501294@qq.com>
2023-01-19 00:09:37 +0800

    add day26 Entering numbers with Stepper

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by xhm on 2023/1/18.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
