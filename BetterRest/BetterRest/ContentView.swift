547ced680890b5c52859edc9208b8c5902ae9988
hongmaoxiao <769501294@qq.com>
2023-01-20 21:53:49 +0800

    add day26 Working with dates

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -17,8 +17,27 @@ struct ContentView: View {
                 .padding()
             DatePicker("Please enter a date", selection: $wakeUp, in: ...Date.now)
                 .labelsHidden()
+            
+            Text(Date.now, format: .dateTime.hour().minute().second())
+            Text(Date.now, format: .dateTime.second().minute().hour())
+            Text(Date.now, format: .dateTime.day().month().year())
+            
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
         
+//        VStack {
+//            Text(Date.now, format: .dateTime.hour().minute().second())
+//            Text(Date.now, format: .dateTime.second().minute().hour())
+//            Text(Date.now, format: .dateTime.day().month().year())
+//
+//            Text(Date.now.formatted(date: .long, time: .shortened))
+//        }
+    }
+    
+    func trivialExample() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
