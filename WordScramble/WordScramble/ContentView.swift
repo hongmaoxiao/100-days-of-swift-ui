63466a5d41dc45da150ac19ea03cda1b9674cdbb
hongmaoxiao <769501294@qq.com>
2023-01-22 18:49:03 +0800

    add day29 Introducing List, your best friend

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by xhm on 2023/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    var body: some View {
+        List {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+
+            Section("Section 3") {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            
+            Section("Section 4") {
+                ForEach(people, id: \.self) {
+                    Text($0)
+                }
+            }
+        }
+        .listStyle(.grouped)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by xhm on 2023/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
