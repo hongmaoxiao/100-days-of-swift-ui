1e5ae77914c10c0a5028839984a5d269b9f7a09f
hongmaoxiao <769501294@qq.com>
2023-01-28 03:01:46 +0800

    add day32 Creating explicit animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,24 +10,52 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
-        )
-        .onAppear {
-            animationAmount = 2
-        }
-       
+//        Button("Tap Me") {
+//            animationAmount += 1
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .overlay(
+//            Circle()
+//                .stroke(.red)
+//                .scaleEffect(animationAmount)
+//                .opacity(2 - animationAmount)
+//                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
+//        )
+//        .onAppear {
+//            animationAmount = 2
+//        }
+        
+//        print(animationAmount)
+//        return VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(
+//                .easeInOut(duration: 1)
+//                .repeatCount(3, autoreverses: true)), in: 1...10)
+//            Spacer()
+//
+//            Button("Tap Me") {
+//                animationAmount += 1
+//            }
+//            .padding(50)
+//            .background(.red)
+//            .foregroundColor(.white)
+//            .clipShape(Circle())
+//            .scaleEffect(animationAmount)
+//        }
+        
+        
+            Button("Tap Me") {
+                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                    animationAmount += 360
+                }
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
