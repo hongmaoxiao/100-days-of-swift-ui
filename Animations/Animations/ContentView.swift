d49256a5d6224b94a0edad4f9c76fa1bdca82ab6
hongmaoxiao <769501294@qq.com>
2023-01-28 17:23:00 +0800

    add day32 Controlling the animation stack

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
+    @State private var enabled = false
     var body: some View {
 //        Button("Tap Me") {
 //            animationAmount += 1
@@ -46,16 +47,27 @@ struct ContentView: View {
 //        }
         
         
-            Button("Tap Me") {
-                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                    animationAmount += 360
-                }
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+//            Button("Tap Me") {
+//                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+//                    animationAmount += 360
+//                }
+//            }
+//            .padding(50)
+//            .background(.red)
+//            .foregroundColor(.white)
+//            .clipShape(Circle())
+//            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
+        Button("Tap Me") {
+            enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .animation(nil, value: enabled)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+        
     }
 }
 
