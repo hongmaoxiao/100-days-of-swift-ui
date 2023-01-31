8245f8ac8faaf77fada963037177dfec1fd39b11
hongmaoxiao <769501294@qq.com>
2023-02-01 04:48:40 +0800

    add day38 Resizing images to fit the screen using GeometryReader

---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by xhm on 2023/2/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        GeometryReader { geo in
+            Image("Example")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geo.size.width * 0.8)
+                .frame(width: geo.size.width, height: geo.size.height)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by xhm on 2023/2/1.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
