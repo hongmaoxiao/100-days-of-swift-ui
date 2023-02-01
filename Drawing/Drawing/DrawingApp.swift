a8342c3c5db7d323f70a39c24292daf223a43c61
hongmaoxiao <769501294@qq.com>
2023-02-02 04:28:41 +0800

    add day43 Creating custom paths with SwiftUI

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by xhm on 2023/2/2.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
