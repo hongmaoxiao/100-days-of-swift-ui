b6ac80673cf5de78aa6ba2ce36cc40765663ae74
hongmaoxiao <769501294@qq.com>
2023-02-05 02:10:50 +0800

    add day49 Loading an image from a remote server

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -38,17 +38,30 @@ struct Result: Codable {
 struct ContentView: View {
     @State private var results = [Result]()
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
+//        List(results, id: \.trackId) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.trackName)
+//                    .font(.headline)
+//
+//                Text(item.collectionName)
+//            }
+//        }
+//        .task {
+//            await loadData()
+//        }
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
             }
         }
-        .task {
-            await loadData()
-        }
+        .frame(width: 200, height: 200)
     }
     
     func loadData() async {
